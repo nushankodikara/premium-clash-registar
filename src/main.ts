9c5212d98cb082df2bd998583d8fd82c2e79dffd
Nushan Kodikara <nushankodi@gmail.com>
2022-05-27 22:25:15 +0530

    change

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import App from "./App.svelte";
 import { uid } from "./store.js";
 
 function onTelegramAuth(user) {
+    alert(user.id);
+    console.log("onTelegramAuth", user);
     uid.set(user.id);
 }
 
