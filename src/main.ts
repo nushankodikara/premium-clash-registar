db249239ad3e5d961849bf5a09a4c7d3e4cd56b9
Nushan Kodikara <nushankodi@gmail.com>
2022-05-27 22:00:09 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@ import App from "./App.svelte";
 
 let uid = 0;
 
+function onTelegramAuth(user) {
+    alert(
+        "Logged in as " +
+            user.first_name +
+            " " +
+            user.last_name +
+            " (" +
+            user.id +
+            (user.username ? ", @" + user.username : "") +
+            ")"
+    );
+
+    uid = user.id;
+}
+
 const app = new App({
     target: document.getElementById("app"),
 });
