59fd50e59186789e7f1538fbb72efcbb7a7afe51
Nushan Kodikara <nushankodi@gmail.com>
2022-05-28 06:51:11 +0530

    added new cookie function

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { uid } from "./store.js";
 
 let cookies = document.cookie.split("; ");
 
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+    console.log("Cookie got");
+}
+
 // check if the cookie exists
 if (cookies.includes("telegram_user_id")) {
-    // get the uid from the cookie
-    let token = parseInt(
-        cookies
-            .find((cookie) => cookie.startsWith("telegram_user_id="))
-            .split("=")[1]
-    );
-
-    // set the uid in the app
-    uid.set(token);
+    console.log("cookie exists");
+    uid.set(parseInt(getCookie("telegram_user_id")));
 }
 
 const app = new App({
