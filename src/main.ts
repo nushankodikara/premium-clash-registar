2bec75adc21aba9d24f358a94697f07d88c40da8
Nushan Kodikara <nushankodi@gmail.com>
2022-05-28 06:36:11 +0530

    cookies implamented in svelte

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import App from "./App.svelte";
+import { uid } from "./store.js";
 
 const app = new App({
     target: document.getElementById("app"),
 });
 
 let cookies = document.cookie.split("; ");
-console.log(cookies);
+
+// check if the cookie exists
+if (cookies.includes("telegram_user_id")) {
+    // get the uid from the cookie
+    let token = parseInt(
+        cookies
+            .find((cookie) => cookie.startsWith("telegram_user_id="))
+            .split("=")[1]
+    );
+
+    // set the uid in the app
+    uid.set(token);
+}
 
 export default app;
