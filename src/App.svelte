4bfa44ee62ea58ec21137130b0c450a129c46881
Nushan Kodikara <nushankodi@gmail.com>
2022-05-28 18:27:05 +0530

    added few contents and login message

--- src/App.svelte
+++ src/App.svelte
@@ -8,13 +8,33 @@
     console.log("UID From App: ", userID);
 </script>
 
-<div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Welcome {userID}</h6>
-    </div>
-    <div class="card-body">
-        The styling for this basic card example is created by using default
-        Bootstrap utility classes. By using utility classes, the style of the
-        card component can be easily modified with no need for any custom CSS!
+<div class="container">
+    <div id="row">
+        {#if userID}
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">
+                        Welcome {userID}
+                    </h6>
+                </div>
+                <div class="card-body">
+                    You can use this page to manage your clash configuration.
+                    But first let's do a speed test to identify your internet
+                    speed.
+                </div>
+            </div>
+        {:else}
+            <div class="card shadow mb-4 col-md-6">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">
+                        Hello There!
+                    </h6>
+                </div>
+                <div class="card-body">
+                    Please use the "Log in with Telegram" button top right to
+                    login to your telegram account
+                </div>
+            </div>
+        {/if}
     </div>
 </div>
